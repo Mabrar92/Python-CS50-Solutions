466d5268d6648f6c4ec091f58445b8bf05f71cac
Mabrar92 <mabrar92@hotmail.com>
2022-12-30 12:41:27 +0000

    Fri, Dec 30, 2022, 12:41 PM +00:00

---
+++ meal/meal.py
@@ -0,0 +1,18 @@
+"""
+breakfast---  7:00 and 8:00
+lunch ---    12:00 and 13:00
+dinner ---   18:00 and 19:00
+
+"""
+
+def main():
+
+
+
+
+def convert(time):
+
+
+
+if __name__ == "__main__":
+    main()
