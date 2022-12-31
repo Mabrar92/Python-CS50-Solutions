cde933ef933721b27afe38ed7f7febb5c043a84d
Mabrar92 <mabrar92@hotmail.com>
2022-12-31 18:45:35 +0000

    Sat, Dec 31, 2022, 6:45 PM +00:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,1 +1,6 @@
-# A Coke Machine accepts only 5, 10, 25 cents.
+# A Coke Machine accepts only 5, 10, 25 cents. and in these input cases subtract from 50 until 0.
+
+due = 50
+
+while due > 0:
+    coin = int(input("insert coin: ))
