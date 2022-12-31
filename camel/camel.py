52cab8c127eea075552a8bc16600de558b5df675
Mabrar92 <mabrar92@hotmail.com>
2022-12-31 17:28:44 +0000

    Sat, Dec 31, 2022, 5:28 PM +00:00

--- camel/camel.py
+++ camel/camel.py
@@ -5,7 +5,7 @@ name = input("camelCase:").strip()
 for capital in name:
     if capital.isupper():
         print(capital)
-        a= name.split(capital)
+        a= name.partition(capital)
 
 print(name)
 print(a)
