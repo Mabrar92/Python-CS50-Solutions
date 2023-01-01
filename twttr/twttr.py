d1bf4e3df3ab399fe6ed1fcfbe34b059a41fa2ef
Mabrar92 <mabrar92@hotmail.com>
2023-01-01 10:57:00 +0000

    Sun, Jan 1, 2022, 10:57 AM +00:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,6 +1,8 @@
 
 userinput = input("Input: ").strip()
 
+a= ["a","A",]
+
 for chr in userinput :
     if chr == "a" or chr == "i" or chr ==  "o" or chr == "u" or chr=="e":
         userinput = userinput.replace(chr,"")
