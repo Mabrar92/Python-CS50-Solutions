879901c5230d263f6aed49b056a2e3d9738d9c24
Mabrar92 <mabrar92@hotmail.com>
2023-01-01 11:04:53 +0000

    Sun, Jan 1, 2022, 11:04 AM +00:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -4,8 +4,8 @@ userinput = input("Input: ").strip()
 a = ["a","A","e","E","i","I","o","O","u","U"]
 
 for chr in userinput :
-    for vowel in range(len(a)):
-        if chr == vowel :
+    for vowel in a:
+        if chr == vowel:
             userinput = userinput.replace(chr,"")
         else :
             continue
