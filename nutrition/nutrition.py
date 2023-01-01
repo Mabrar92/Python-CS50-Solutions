90ee483f3a731bb0e3ce1f02bf015a1842b99bfd
Mabrar92 <mabrar92@hotmail.com>
2023-01-01 10:30:15 +0000

    Sun, Jan 1, 2022, 10:30 AM +00:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -28,7 +28,7 @@ user_input= input("Item: ").lower().strip()
 for item in food:
     if user_input == item.lower():
         print("Calories:",food[item])
-        break
+
     else:
         continue
 
