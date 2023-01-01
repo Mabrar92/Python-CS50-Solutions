2534811ef1918f126f76f2c0ef0deb56dd2fdd00
Mabrar92 <mabrar92@hotmail.com>
2023-01-01 10:19:57 +0000

    Sun, Jan 1, 2022, 10:19 AM +00:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,6 +1,28 @@
 
 food = {
-    "Apple":
-
-
+    "Apple": 130,
+    "Avocado": 50,
+    "Banana":110,
+    "Cantaloupe":50,
+    "Grapefruit":60,
+    "Grapes":90,
+    "Honeydew":50,
+    "Kiwifruit":90,
+    "Lemon":15,
+    "Lime":20,
+    "Nectarine":60,
+    "Orange":80,
+    "Peach":60,
+    "Pear":100,
+    "Pineapple":50,
+    "Plums":70,
+    "Strawberries":50,
+    "Sweet Cherries":100,
+    "Tangerine":50,
+    "Watermelon":80
 }
+
+user_input= input("Item: ").lower().strip()
+
+for item in food:
+    
