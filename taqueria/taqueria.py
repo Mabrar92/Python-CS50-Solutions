16fc79dacad7776bfdb451985643ee623a382283
Mabrar92 <mabrar92@hotmail.com>
2023-01-03 17:46:52 +0000

    Tue, Jan 3, 2023, 5:46 PM +00:00

--- taqueria/taqueria.py
+++ taqueria/taqueria.py
@@ -17,11 +17,11 @@ while True:
     try:
         user_items = input("Item: ").title()
         if user_items in food_items :
-            a+= food_items[user_items]
+            a+= round(food_items[user_items],2)
 
     except EOFError:
         print("\n")
-        print(f"Total: {a}$")
+        print(f"Total: {a}$",)
     except KeyError:
         pass
 
