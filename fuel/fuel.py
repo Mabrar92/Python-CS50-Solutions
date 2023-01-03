300d46ecba91bf0282fd8024f6cfc55e030b30e1
Mabrar92 <mabrar92@hotmail.com>
2023-01-03 09:35:55 +0000

    Tue, Jan 3, 2023, 9:35 AM +00:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -3,17 +3,18 @@ def main():
     x = get_result()
     print(x)
 
+
 def get_result():
 
     while True:
         try:
-            x = int(input("Fraction"))
+            x = input("Fraction")
 
         except (ValueError,ZeroDivisionError) :
             pass
 
         else:
-            split_string= x.split("/")
+            split_string = x.split("/")
             num =  split_string[0]
             denum = split_string[1]
             return split_string
