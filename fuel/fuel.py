a983a2f9ab98c38cbfe3c04813df75dada002aee
Mabrar92 <mabrar92@hotmail.com>
2023-01-03 09:37:18 +0000

    Tue, Jan 3, 2023, 9:37 AM +00:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -8,7 +8,7 @@ def get_result():
 
     while True:
         try:
-            x = input("Fraction")
+            x = int(input("Fraction "))
 
         except (ValueError,ZeroDivisionError) :
             pass
