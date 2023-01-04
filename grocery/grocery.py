af5ffe452b5e994d84ba53b4385dba20078f43d9
Mabrar92 <mabrar92@hotmail.com>
2023-01-04 11:51:00 +0000

    Wed, Jan 4, 2023, 11:51 AM +00:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -20,7 +20,8 @@ while True:
 
         #Output the values along with Count as prefix.
         for item in grocery_list:
-            print(list.count(item),item)
+            print(item)
+            #print(list.count(item),item)
 
         break
 
