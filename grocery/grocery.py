4c7ab88e4f583a3b835b88f405a5dcfa714b40c9
Mabrar92 <mabrar92@hotmail.com>
2023-01-04 10:21:03 +0000

    Wed, Jan 4, 2023, 10:21 AM +00:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -3,5 +3,13 @@
 dict a={}
 
 try :
+    # Get the grocery list from the user
     user_input= input()
-    
+
+    # Store each item in the dict on the go
+
+except EOFError:
+    #Sort Dict Values Alphabetically
+
+except KeyError:
+    pass
