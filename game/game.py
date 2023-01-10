96483b283c7d53e9ba7daaffacb63450bd2f3c5c
Mabrar92 <mabrar92@hotmail.com>
2023-01-10 19:29:44 +0000

    Tue, Jan 10, 2023, 7:29 PM +00:00

--- game/game.py
+++ game/game.py
@@ -1,22 +1,23 @@
 import random
 
 user_level = int(input("Level: "))
-random.randint(0,user_level)
+level=random.randint(0,user_level)
 while True:
     try:
         if user_level < 0:
             continue
         else:
-            user_guess= input("Guess: ")
+            user_guess= int(input("Guess: "))
 
             if user_guess > 0 :
-                if user_guess > user_level:
-                    print()
-                elif user_guess < user_level:
-
-
+                if user_guess > level:
+                    print("Too large!")
+                    continue
+                elif user_guess < level:
+                     print("Too small!")
+                     continue
                 else:
-                    print("just Right!")
+                    print("Just right!")
                     break
         break
     except ValueError:
