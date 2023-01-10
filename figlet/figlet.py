a3333a0c9e66ef334b3bb79f8514865c3ceb1536
Mabrar92 <mabrar92@hotmail.com>
2023-01-10 09:24:52 +0000

    Tue, Jan 10, 2023, 9:24 AM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -9,16 +9,18 @@ import random
 
 
 figlet = Figlet()
-user_input = input("input:")
+
 fonts= figlet.getFonts()
 
 
 if len(sys.argv) == 1 :
+    user_input = input("input:")
     ran = random.randint(0,len(fonts))
     figlet.setFont(font=fonts[ran])
     print(figlet.renderText(user_input))
 
 elif len(sys.argv) > 2 and (sys.argv[1] == "-f" or sys.argv[2] == "-font") :
+    user_input = input("input:")
     if sys.argv[2] in fonts:
         figlet.setFont(font=sys.argv[2])
         print(figlet.renderText(user_input))
