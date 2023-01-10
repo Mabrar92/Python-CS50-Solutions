ed052f60cee1f65df53c8fad61539a8264c0b255
Mabrar92 <mabrar92@hotmail.com>
2023-01-10 18:16:21 +0000

    Tue, Jan 10, 2023, 6:16 PM +00:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -9,7 +9,7 @@ while True:
         user_input = input("Name: ")
         a[i] = user_input
         i+=1
-
+        name_count = len(i)
     except EOFError:
         print("\n Adieu, adieu, to",list(a.values()))
         break
