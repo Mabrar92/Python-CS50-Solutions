1b929b629e8ea65c92a52f360851ba9d530c7848
Mabrar92 <mabrar92@hotmail.com>
2023-01-10 19:26:08 +0000

    Tue, Jan 10, 2023, 7:26 PM +00:00

--- game/game.py
+++ game/game.py
@@ -1,11 +1,24 @@
+import random
 
-
+user_level = int(input("Level: "))
 
 while True:
     try:
-        user_num = int(input("Level: "))
-        
+        if user_level < 0:
+            continue
+        else:
+            random.randint(0,user_level)
+            user_guess= input("Guess: ")
 
+            if user_guess > 0 :
+                if user_guess > user_level:
+                    print()
+                elif user_guess < user_level:
+
+
+                else:
+                    print("just Right!")
+                    break
         break
     except ValueError:
         pass
