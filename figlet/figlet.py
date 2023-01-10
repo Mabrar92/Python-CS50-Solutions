b81f90ce01828496d5149ddca734e9f386ba8ba4
Mabrar92 <mabrar92@hotmail.com>
2023-01-10 08:55:42 +0000

    Tue, Jan 10, 2023, 8:55 AM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -4,3 +4,17 @@
 import sys
 from pyfiglet import figlet
 
+
+
+
+
+figlet = Figlet()
+
+#for list of available fonts
+figlet.getFonts()
+
+#f is the font name as str
+figlet.setFont(font=f)
+
+# render the final output
+print(figlet.renderText(s))
