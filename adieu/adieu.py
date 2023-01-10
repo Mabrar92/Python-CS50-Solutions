30a0bf58dd5ba4b402039652edd70dcccef1c8b8
Mabrar92 <mabrar92@hotmail.com>
2023-01-10 18:33:47 +0000

    Tue, Jan 10, 2023, 6:33 PM +00:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -11,11 +11,12 @@ while True:
         i+=1
         name_count = len(a)
         name_list = list(a.values())
-        
+
 
 
     except EOFError:
         for i in range(name_count):
+            # i names , i-1 commas
             print("\n Adieu, adieu, to",))
 
         break
