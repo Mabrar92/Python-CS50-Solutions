db89e0e827c8269e839adfed5668c8d258f779fa
Mabrar92 <mabrar92@hotmail.com>
2023-01-12 19:05:37 +0000

    Thu, Jan 12, 2023, 7:05 PM +00:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -7,7 +7,7 @@ try:
     response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
     r=response.json()
 
-    #print(json.dumps(r, indent=2))
+
     for items in r["bpi"].values():
         if items['code']=='USD':
             print(items['rate_float'])
