2e31f6dbcb4d6c7d0515c97ddd4089766d2c7163
Mabrar92 <mabrar92@hotmail.com>
2023-01-12 18:47:12 +0000

    Thu, Jan 12, 2023, 6:47 PM +00:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -4,7 +4,7 @@ import requests
 
 try:
     response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
-    response.json()
-    print(response)
+    r=response.json()
+    print(r)
 except requests.RequestException:
     ...
