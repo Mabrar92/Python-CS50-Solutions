4cf9f9c934c1db43c6b14e46daed62d36264906c
Mabrar92 <mabrar92@hotmail.com>
2023-01-12 19:33:06 +0000

    Thu, Jan 12, 2023, 7:33 PM +00:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -8,7 +8,7 @@ try:
     if len(sys.argv) < 2:
         sys.exit("Missing Command Line Argument")
     elif len(sys.argv) == 2:
-        if sys.argv[1].isnumeric()  :
+        if sys.argv[1].isdigit()  :
             response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
             r=response.json()
             for items in r["bpi"].values():
