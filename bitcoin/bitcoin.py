ad68cf78d41608e95bc3349364cdef331ae68096
Mabrar92 <mabrar92@hotmail.com>
2023-01-12 19:07:15 +0000

    Thu, Jan 12, 2023, 7:07 PM +00:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -1,9 +1,11 @@
 import requests
 import json
+import sys
 
 
 
 try:
+    if len(sys.argv) < 2
     response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
     r=response.json()
 
